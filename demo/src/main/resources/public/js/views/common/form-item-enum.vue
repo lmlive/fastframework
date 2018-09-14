7855e2bbba370c8219bdb9fce4e455836f50f1ff
LM <live_lm@live.cn>
2018-09-14 17:32:45 +0800

    running

---
+++ demo/src/main/resources/public/js/views/common/form-item-enum.vue
@@ -0,0 +1,27 @@
+<template>
+  <span v-if="readonly"> {{txt}}</span>
+  <el-select v-model="dvalue" placeholder="请选择" :loading="loading" v-else>
+    <el-option v-for="item in source" :key="item" :label="item" :value="item">
+    </el-option>
+  </el-select>
+</template>
+<script>
+define(["vue", 'utils/util'], function(Vue, util) {
+
+  return Vue.component("l-form-item-enum", {
+    template: template,
+    props: [  "value", "source", "readonly"],
+    data() {
+      return { dvalue: null, options: [], txt: null,loading:false };
+    },
+    watch: {
+      dvalue: function(v) {
+        this.$emit('input', this.dvalue)
+      }
+    },
+
+
+  });
+});
+</script>
+
---
+++ entitymeta/metaandrepositoryservice/src/main/java/org/livem/meta/EnumMeta.java
@@ -0,0 +1,13 @@
+package org.livem.meta;
+
+public class EnumMeta extends  BaseUiMeta {
+    private  String[] enumSource;
+
+    public String[] getEnumSource() {
+        return enumSource;
+    }
+
+    public void setEnumSource(String[] enumSource) {
+        this.enumSource = enumSource;
+    }
+}
